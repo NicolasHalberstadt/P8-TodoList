5c509afd92ceebb4806e8803c213bd0f5c750f70
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-07-09 23:00:04 +0200

    #4 - User's route role security

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
 use AppBundle\Form\UserType;
+use AppBundle\Form\UserAdminType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,55 +16,78 @@ class UserController extends Controller
      */
     public function listAction()
     {
-        return $this->render('user/list.html.twig', ['users' => $this->getDoctrine()->getRepository('AppBundle:User')->findAll()]);
+        return $this->render(
+            'user/list.html.twig',
+            ['users' => $this->getDoctrine()->getRepository('AppBundle:User')->findAll()]
+        );
     }
-
+    
     /**
      * @Route("/users/create", name="user_create")
      */
     public function createAction(Request $request)
     {
         $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-
+        $form = null;
+        if ($this->getUser()) {
+            $isAdmin = strpos($this->getUser()->getRole(), "ROLE_ADMIN") !== false;
+            if ($isAdmin) {
+                $form = $this->createForm(UserAdminType::class, $user);
+            }
+        } else {
+            $form = $this->createForm(UserType::class, $user);
+        }
+        
         $form->handleRequest($request);
-
+        
         if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
             $user->setPassword($password);
-
+            $user->setRole('ROLE_USER');
+            
             $em->persist($user);
             $em->flush();
-
+            
             $this->addFlash('success', "L'utilisateur a bien été ajouté.");
-
+            
             return $this->redirectToRoute('user_list');
         }
-
+        
         return $this->render('user/create.html.twig', ['form' => $form->createView()]);
     }
-
+    
     /**
      * @Route("/users/{id}/edit", name="user_edit")
      */
     public function editAction(User $user, Request $request)
     {
-        $form = $this->createForm(UserType::class, $user);
-
+        $form = null;
+        if ($this->getUser()) {
+            $isAdmin = strpos($this->getUser()->getRole(), "ROLE_ADMIN") !== false;
+            if ($user == $this->getUser()) {
+                $form = $this->createForm(UserType::class, $user, ['user' => $user]);
+            } elseif ($isAdmin) {
+                $form = $this->createForm(UserAdminType::class, $user, ['user' => $user]);
+            }
+        }
+        
         $form->handleRequest($request);
-
+        
         if ($form->isValid()) {
-            $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
-            $user->setPassword($password);
-
+            /*dump($form->getData()->getPassword());
+            exit;
+            if ($request->get('password')) {
+            
+            }*/
+         /*   $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);*/
             $this->getDoctrine()->getManager()->flush();
-
             $this->addFlash('success', "L'utilisateur a bien été modifié");
-
+            
             return $this->redirectToRoute('user_list');
         }
-
+        
         return $this->render('user/edit.html.twig', ['form' => $form->createView(), 'user' => $user]);
     }
 }
