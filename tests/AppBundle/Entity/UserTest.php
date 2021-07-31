6bbbedf8c3acda74ca177611eaf57616bffb1400
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-07-31 18:29:39 +0200

    #5 - tests user entity

---
+++ tests/AppBundle/Entity/UserTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use AppBundle\Entity\User;
+
+/**
+ * Class UserTest
+ *
+ * @author Nicolas Halberstadt <halberstadtnicolas@gmail.com>
+ */
+class UserTest extends TestCase
+{
+    public function testUserCreate()
+    {
+        $user = new User();
+        $user->setUsername('Username_test');
+        $user->setEmail('email@test.com');
+        $user->setPassword('test_password');
+        $user->setTasks([]);
+        $this->assertEquals(null, $user->getId());
+        $this->assertEquals('Username_test', $user->getUsername());
+        $this->assertEquals('email@test.com', $user->getEmail());
+        $this->assertIsArray($user->getRoles());
+        $this->assertContains('ROLE_USER', $user->getRoles());
+        $this->assertEquals(null, $user->getSalt());
+        $this->assertEmpty($user->getTasks());
+        $this->assertEquals('test_password', $user->getPassword());
+    }
+    
+    public function testUpdateUserRole()
+    {
+        $user = new User();
+        $user->setRoles(['ROLE_ADMIN']);
+        $this->assertContains('ROLE_ADMIN', $user->getRoles());
+    }
+}
