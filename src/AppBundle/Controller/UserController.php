bda3ec96eb880794b4cdd85d21bddabfb4e40de9
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-07-31 18:04:29 +0200

    #4 - user update and delete method

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -28,24 +28,12 @@ class UserController extends Controller
     public function createAction(Request $request)
     {
         $user = new User();
-        $form = null;
-        if ($this->getUser()) {
-            $isAdmin = strpos($this->getUser()->getRole(), "ROLE_ADMIN") !== false;
-            if ($isAdmin) {
-                $form = $this->createForm(UserAdminType::class, $user);
-            }
-        } else {
-            $form = $this->createForm(UserType::class, $user);
-        }
-        
+        $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
-        
-        if ($form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
             $user->setPassword($password);
-            $user->setRole('ROLE_USER');
-            
             $em->persist($user);
             $em->flush();
             
@@ -62,26 +50,11 @@ class UserController extends Controller
      */
     public function editAction(User $user, Request $request)
     {
-        $form = null;
-        if ($this->getUser()) {
-            $isAdmin = strpos($this->getUser()->getRole(), "ROLE_ADMIN") !== false;
-            if ($user == $this->getUser()) {
-                $form = $this->createForm(UserType::class, $user, ['user' => $user]);
-            } elseif ($isAdmin) {
-                $form = $this->createForm(UserAdminType::class, $user, ['user' => $user]);
-            }
-        }
-        
+        $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
-        
-        if ($form->isValid()) {
-            /*dump($form->getData()->getPassword());
-            exit;
-            if ($request->get('password')) {
-            
-            }*/
-         /*   $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
-            $user->setPassword($password);*/
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
             $this->getDoctrine()->getManager()->flush();
             $this->addFlash('success', "L'utilisateur a bien été modifié");
             
@@ -89,5 +62,19 @@ class UserController extends Controller
         }
         
         return $this->render('user/edit.html.twig', ['form' => $form->createView(), 'user' => $user]);
+    }
+    
+    /**
+     * @Route("/users/{id}/delete", name="user_delete")
+     */
+    public function deleteAction(User $user)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($user);
+        $em->flush();
+        
+        $this->addFlash('success', "L'utilisateur a bien ete supprimé");
+        
+        return $this->redirectToRoute('user_list');
     }
 }
