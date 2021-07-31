d1336b1d63dba283721c9f9894a829d3a95ab2ef
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-07-31 18:09:42 +0200

    #5 - tests defaultController

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -9,10 +9,8 @@ class DefaultControllerTest extends WebTestCase
     public function testIndex()
     {
         $client = static::createClient();
-
         $crawler = $client->request('GET', '/');
-
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertContains('Bienvenue sur Todo List', $crawler->filter('.col-md-12 h1')->text());
     }
 }
