d0ad586cef1815b2b97b942665b6eb30fefcb77e
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-07-31 18:30:05 +0200

    #4 - roles choice on user form

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -4,6 +4,7 @@ namespace AppBundle\Form;
 
 use AppBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -16,23 +17,33 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', TextType::class, ['label' => "Nom d'utilisateur"]);
-            if ($options['user'] == null) {
-                $builder->add(
-                    'password',
-                    RepeatedType::class,
-                    [
-                        'type' => PasswordType::class,
-                        'invalid_message' => 'Les deux mots de passe doivent correspondre.',
-                        'required' => true,
-                        'first_options' => ['label' => 'Mot de passe'],
-                        'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
-                    ]
-                );
-            }
-            
-            $builder->add('email', EmailType::class, ['label' => 'Adresse email']);
+            ->add('username', TextType::class, ['label' => "Nom d'utilisateur"])
+            ->add('email', EmailType::class, ['label' => 'Adresse email'])
+            ->add(
+                'password',
+                RepeatedType::class,
+                [
+                    'type' => PasswordType::class,
+                    'invalid_message' => 'Les deux mots de passe doivent correspondre.',
+                    'required' => true,
+                    'first_options' => ['label' => 'Mot de passe'],
+                    'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
+                ]
+            )
+            ->add(
+                'roles',
+                ChoiceType::class,
+                [
+                    'multiple' => true,
+                    'expanded' => true,
+                    'choices' => [
+                        'User' => 'ROLE_USER',
+                        'Admin' => 'ROLE_ADMIN',
+                    ],
+                ]
+            );;
     }
+    
     
     public function configureOptions(OptionsResolver $resolver)
     {
