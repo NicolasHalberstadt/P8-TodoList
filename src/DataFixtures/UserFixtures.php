83f16cc58f20a394c5cb315945f3698592f72eb5
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-09-12 20:49:41 +0200

    #12 - Task and User fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    
+    public function load(ObjectManager $manager)
+    {
+        $admin = new User();
+        $admin->setUsername('Admin');
+        $admin->setEmail('admin@todolist.com');
+        $admin->setPassword($this->passwordEncoder->encodePassword($admin, 'compteAdmin'));
+        $admin->setRoles(['ROLE_ADMIN']);
+        $manager->persist($admin);
+        
+        $user = new User();
+        $user->setUsername('User');
+        $user->setEmail('user@todolist.com');
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'compteUser'));
+        $manager->persist($user);
+        
+        $manager->flush();
+    }
+}
