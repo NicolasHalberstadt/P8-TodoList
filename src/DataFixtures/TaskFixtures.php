e4896bae3507117d01cf85f22f1d78b6712a08b8
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-09-27 19:34:12 +0200

    #12 - fixtures order

--- src/DataFixtures/TaskFixtures.php
+++ src/DataFixtures/TaskFixtures.php
@@ -4,9 +4,11 @@
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Task;
 use App\Entity\User;
+use App\DataFixtures\UserFixtures;
 use Faker\Factory;
 
 /**
@@ -15,7 +17,7 @@ use Faker\Factory;
  * @author Nicolas Halberstadt <halberstadtnicolas@gmail.com>
  * @package App\UserFixtures
  */
-class TaskFixtures extends Fixture
+class TaskFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
@@ -35,5 +37,12 @@ class TaskFixtures extends Fixture
             $manager->persist($task);
         }
         $manager->flush();
+    }
+    
+    public function getDependencies()
+    {
+        return [
+            UserFixtures::class,
+        ];
     }
 }
